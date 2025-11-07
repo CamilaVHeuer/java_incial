eeaa4490d2fead1297af053983f71880bed64fc3
camilavheuer <cbvillalbaheuer@gmail.com>
2025-11-07 10:04:38 -0300

    first commit

---
+++ HolaMundo/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaracion de variables
+        int numero = 10;
+        String nombre = "Camila";
+        char letra = 'A';
+        double temperatura = 36.5;
+        Boolean SioNo = true;
+        long poblaccion = 7800000000L;
+        System.out.println("Numero: " + numero);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Letra: " + letra);
+        System.out.println("Temperatura: " + temperatura);
+        System.out.println("Booleano: " + SioNo);
+        System.out.println("Poblacion mundial: " + poblaccion);
+        /*
+         * Fin de la declaracion de variables
+         * y de paso estoy probando hacer comentarios multinlinea
+         */
+    }
+
+}
